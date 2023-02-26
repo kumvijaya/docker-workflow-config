45f2d05015099f62ba6797345e445d3a2529626f
vijayakumar.ayyavoo@toyota.com <vijayakumar.ayyavoo@toyota.com>
2023-02-26 18:04:37 +0530

    loop logic

--- build.sh
+++ build.sh
@@ -7,23 +7,24 @@ user=$2
 password=$3
 tags=$4
 registry=$5
-# read
-while IFS="=" read -r key value; do content["$key"]=$value; done < <(
-  yq '.fileImageMap | to_entries | map([.key, .value] | join("=")) | .[]' $buildfile
-)
 
 docker_registry='' 
 if [ "$registry" != 'EMPTY' ]; then
     docker_registry=$registry
 fi
 
-echo ${password} | docker login --username ${user} --password-stdin ${docker_registry}
+# read
+while IFS="=" read -r key value; do content["$key"]=$value; done < <(
+  yq '.fileImageMap | to_entries | map([.key, .value] | join("=")) | .[]' $buildfile
+)
 
 for key in "${!content[@]}"; do 
   printf "key %s, value %s\n" "$key" "${content[$key]}";
-  dockerfile=$key
-  image=${content[$key]}  
-  docker build -f ${dockerfile} ${tags} .
+  imagefile=$key
+  docker_dir=$(python docker-workflow-config/get_dir.py --path $imagefile)
+  docker_file=$(python docker-workflow-config/get_file.py --path $imagefile)
+  image=${content[$key]}
+  echo ${password} | docker login --username ${user} --password-stdin ${docker_registry}
+  docker build -f ${docker_file} ${tags} ${docker_dir}
   docker push ${image} --all-tags 
 done
-

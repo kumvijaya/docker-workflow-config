d4725480f9e87225519da4b2b761eade4ac7d811
vijayakumar.ayyavoo@toyota.com <vijayakumar.ayyavoo@toyota.com>
2023-02-26 12:42:36 +0530

    loop logic

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ declare -A content
 
 # read
 while IFS="=" read -r key value; do content["$key"]=$value; done < <(
-  yq 'to_entries | map([.key, .value] | join("=")) | .[]' docker-workflow-config/build.yaml
+  yq '.fileImageMap | to_entries | map([.key, .value] | join("=")) | .[]' docker-workflow-config/build.yaml
 )
 
-for key in "${!content[@]}"; do printf "key %s, value %s\n" "$key" "${content[$key]}"; done
+for key in "${!content[@]}"; do 
+  printf "key %s, value %s\n" "$key" "${content[$key]}"; 
+done

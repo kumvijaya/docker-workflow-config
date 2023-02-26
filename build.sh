07294b39135a347f4670f6ab004a906b1934d864
vijayakumar.ayyavoo@toyota.com <vijayakumar.ayyavoo@toyota.com>
2023-02-26 13:44:50 +0530

    loop logic

--- build.sh
+++ build.sh
@@ -3,11 +3,26 @@
 declare -A content
 
 buildfile=$1
+user=$2
+password=$3
+tags=$4
+registry=$5
 # read
 while IFS="=" read -r key value; do content["$key"]=$value; done < <(
   yq '.fileImageMap | to_entries | map([.key, .value] | join("=")) | .[]' $buildfile
 )
 
+docker_registry='' 
+if [ "$registry" != 'EMPTY' ]; then
+    docker_registry=$registry
+fi
+
 for key in "${!content[@]}"; do 
-  printf "key %s, value %s\n" "$key" "${content[$key]}"; 
+  printf "key %s, value %s\n" "$key" "${content[$key]}";
+  dockerfile=$key
+  image=${content[$key]}
+  echo ${password} | docker login --username ${user} --password-stdin ${docker_registry}
+  docker build -f ${dockerfile} ${tags} .
+  docker push ${image} --all-tags 
 done
+
